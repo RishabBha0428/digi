463e3c0e8988b87815daf86e57522d1bf18764e1
RishabBha0428 <rishabstar04@gmail.com>
2023-11-09 14:17:18 -0800

    PR

--- gpt/catalog/__init__.py
+++ gpt/catalog/__init__.py
@@ -1,38 +1,50 @@
-#created fork#created fork
+#created fork
+from kubernetes import client, config
 import numpy as np
-catalog = [
-    {"input": "lounge", "output": ["lamp", "oven", "building", "rooms"]},
-    {"input": "airport", "output": ["plane", "speaker", "cargo"]},
 
-    {"input": "office", "output": ["fan", "phone", "plug", "underdesk"]},
+
+# Load kubeconfig file
+config.load_kube_config()
+
+def create_catalog():
+# Create an instance of the API class
+#Code to get all the running pods, but how do I format this into the example above
+#for example, I assumed that the input would be a space, and the output would be a series of digis within that space
+#is that a right assumption, or are we looking for more
+    #code to get the running space as well?
+    v1 = client.CoreV1Api()
+
+    namespace = 'default' 
+    catalog =  {}
+
+    pods = v1.list_namespaced_pod(namespace)
+    for pod in pods.items:
+        if pod.status.phase == "Running":
+            name = pod.metadata.name
+            filtered_name = name[:name.index('-')]
+            catalog[filtered_name] = name
+    return catalog
+
+catalog = create_catalog()
+
+#example database of examples, can be generated prettu simply by looking at yaml file and feeding to gpt to generate questions, or knowing predetermined questions
+examples = [
+    {"input": "Turn on the lamp 1", "output": "l1"},
+    {"input": "Lower the brightness on lammp 1", "output": "l1"},
+    {"input": "Turn off the lights in my main room", "output": "l1"},
+    {"input": "Lower the temperature in my room", "output": "l1"}
 ]
 
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import faiss
 from langchain.prompts import SemanticSimilarityExampleSelector
 
-to_vectorize = [" ".join(digi.values()) for digi in catalog]
+to_vectorize = [" ".join(digi.values()) for digi in examples]
 embeddings = OpenAIEmbeddings()
-vectorstore = faiss.from_texts(to_vectorize, embeddings, metadatas=catalog)
+vectorstore = faiss.from_texts(to_vectorize, embeddings, metadatas=examples)
 
 example_selector = SemanticSimilarityExampleSelector(
     vectorstore=vectorstore,
     k=2,
 )
-
 example_selector.select_examples({"input": "turn on the room"})["digi"]
-
-
-"""
-from kubernetes import client, config
-
-config.load_incluster_config()()
-
-v1=client.CoreV1Api()
-print("Listing pods with their IPs:")
-ret = v1.list_pod_for_all_namespaces(watch=False)
-for i in ret.items:
-    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-
-"""
-
